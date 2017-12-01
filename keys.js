bfe6a504344fcf81b778eb4d1dc699dbc9319c4b
Shant Wanes <swanes@bjsrestaurants.com>
2017-11-30 21:29:27 -0800

    liri I

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'MWyyG4K0K4ctn0DRqhhWnYU3L',
+  consumer_secret: 'RU0XE0WugnWXNQkdMTT2hbtd9KXBGfHO2amLnZ1fTc1eeX40Ff',
+  access_token_key: '936440876070354944-4d4luJBv4qu5JYmhuyKAigRvzd33det',
+  access_token_secret: 'nvG3FOlgZ4bQrQDrsrKwGeQShef3qeSIqVM7xBbHOdEML',
+}
+
+module.exports = { twitterKeys : twitterKeys}
---
+++ liri.js
@@ -0,0 +1,7 @@
+var keys = require("./keys.js");
+
+var consumer_key = keys.twitterKeys.consumer_key;
+var consumer_secret = keys.twitterKeys.consumer_secret;
+var access_token_key = keys.twitterKeys.access_token_key;
+var access_token_secret = keys.twitterKeys.access_token_secret;
+
